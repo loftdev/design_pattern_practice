05ec1ba1fb086edaf847749c1fca13efa9530bba
loftdev <36267607+loftdev@users.noreply.github.com>
2019-07-22 12:48:04 +0900

    code change

--- factory_method.py
+++ factory_method.py
@@ -1,37 +1,33 @@
-class PhoneCreator:
-    def create(self):
-        print("This is phone creator")
+from abc import ABC
 
 
-class Iphone(PhoneCreator):
+class PhoneFactory(ABC):
+    def __init__(self, os):
+        self.os = os
+
     def build(self):
-        print("new iPhone is made")
 
-
-class SamsungGalaxy(PhoneCreator):
-    def build(self):
-        print("new Samsung Galaxy is made")
-
-
-class PhoneFactory:
-    @staticmethod
-    def get_phone(os):
-        if os == "ios":
-            return Iphone().build()
-        elif os == "android":
-            return SamsungGalaxy().build()
+        if self.os == "ios":
+            IphoneBuilder(self.os).build()
+        elif self.os == "android":
+            SamsungBuilder(self.os).build()
         else:
-            print("We are not making that phone yet")
+            print("We are not yet creating that phone")
+
+
+class IphoneBuilder(PhoneFactory):
+    def build(self):
+        print("New iPhone made ")
+
+
+class SamsungBuilder(PhoneFactory):
+    def build(self):
+        print("New Samsung made")
 
 
 # test code
 
-client1 = PhoneFactory()
-print("client 1")
-client1.get_phone("ios")
-client2 = PhoneFactory()
-print("Client 2")
-client2.get_phone("android")
-
-
-
+client1 = PhoneFactory("ios")
+print(client1.build())
+client2 = PhoneFactory("android")
+print(client2.build())
